f9c6052d7282dd2624ade7b6deb23c3ebbce26d1
andrecamargotii <andrecamargotii@gmail.com>
2020-08-21 11:37:14 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
