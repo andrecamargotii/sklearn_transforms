9f58c4cd7a862afb6e2d9b88998acd15ad9188db
andrecamargotii <andrecamargotii@gmail.com>
2020-08-21 23:38:00 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.preprocessing import StandardScaler
+import pandas as pd
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -24,10 +24,20 @@ class ReplaceDfNaN(BaseEstimator, TransformerMixin):
         data = X.copy()
         return data.fillna(self.nan)
 
-class NormFeatures(BaseEstimator, TransformerMixin):
+class NewFeatures(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
         return self
     def transform(self, X):
         data = X.copy()
-        scaler = StandardScaler()
-        return scaler.fit_transform(data)
+        data['MEDIA'] =  data.iloc[:, 0:4].mean(axis=1)
+        data['RENDIMENTO'] = (data <= 6).astype(int).sum(axis=1)
+        return data
+class NormFeatures, TransformerMixin):
+    def __init__(self, feature)
+        self.feature = feature
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X):
+        data = X.copy()
+        data[data[1:4] > 10] = 10
+        return data
